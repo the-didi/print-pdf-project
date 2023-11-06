b5b3e1ee0019df5d242f8e209d2881f2e342824d
Little-Guo <61458828+the-didi@users.noreply.github.com>
2023-11-06 15:32:30 +0800

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,45 @@
+import { join } from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePluginDoubleshot } from 'vite-plugin-doubleshot'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: join(__dirname, 'src/render'),
+  plugins: [
+    vue(),
+    VitePluginDoubleshot({
+      type: 'electron',
+      main: 'dist/main/index.js',
+      entry: 'src/main/index.ts',
+      outDir: 'dist/main',
+      external: ['electron'],
+      electron: {
+        build: {
+          config: './electron-builder.config.js',
+        },
+        preload: {
+          entry: 'src/preload/index.ts',
+          outDir: 'dist/preload',
+        },
+      },
+      configureForMode: (config, mode) => {
+        if (mode !== 'production')
+          config.debug = true
+
+        return config
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@render': join(__dirname, 'src/render'),
+      '@main': join(__dirname, 'src/main'),
+    },
+  },
+  base: './',
+  build: {
+    outDir: join(__dirname, 'dist/render'),
+    emptyOutDir: true,
+  },
+})
